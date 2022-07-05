b071e13ed4b3fc5d91b65d58108974c68bd957e1
Luke <pilot@Luke.local>
2022-07-05 18:21:56 +0300

    part3 3.6 done

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let person = {
+	id: 1,
+	name: 'Arto Hellas',
+	number: '040-123456',
+};
+
+let persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+];
+
+app.get('/api/persons', (request, response) => {
+	response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+	let amount = persons.length;
+	let date = Date(response);
+	response.send(
+		'<p>Phonebook has info for ' + amount + ' people</p><p>' + date + '</p'
+	);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+	let id = request.params.id;
+	let single = persons.find((person) => person.id === Number(id));
+	response.json(single);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+	response.status(204).end();
+});
+
+let len = persons.length + 1;
+
+const newId = () => {
+	return Math.floor(Math.random(len, 100));
+};
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+	const nam = persons.find((person) => person.name === body.name);
+
+	if(!body.name || !body.number) {
+		return response.status(400).json({
+			error: 'name or number missing',
+		});
+	};
+
+	if(nam) {
+		return response.status(400).json({
+			error: 'name already in use',
+		});
+	};
+
+	const newPerson = {
+		name: body.name,
+		number: body.number,
+		id: newId(),
+	};
+
+	persons = persons.concat(newPerson);
+	response.json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
